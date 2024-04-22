02dd03039dbab716581f3744b33cbf376cbc7e03
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-22 16:40:00 +0100

    adding definitions

---
+++ app.js
@@ -0,0 +1,20 @@
+/* express = function which on calling will add a load of methods to our app 
+variable below. */
+const express = require("express");
+
+const port = 3000;
+/* to app.listen passing in port created in variable above and a callback 
+function. This callback function will be called as soon as the server starts 
+listening */ 
+app.listen(port, () => {
+
+});
+
+///// CALLBACK FUNCTION DEFINITION 
+/* A callback function is a function passed into another function as an argument,
+ which is then invoked inside the outer function to complete some kind of 
+ routine or action */
+ /* Callbacks are often used in asynchronous programming, such as in handling 
+ events, server requests, or delays (like timeouts). */
+
+///// SYNCHRONOUS JAVASCRIPT 
