d2483ef4a167d91a377769ee50b2c05daa00fbc1
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-25 16:42:50 +0100

    content moved into controllers

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+const getAllUsers = (req, res) => {
+  /* for now send back message this route is not implemented */
+  /* still sending back a json object with a status of error and a message */
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,44 +1,6 @@
 /* need to import the express module */
 const express = require('express');
 
-/* All functions needed to be in user router file for now */
-const getAllUsers = (req, res) => {
-  /* for now send back message this route is not implemented */
-  /* still sending back a json object with a status of error and a message */
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
 const router = express.Router();
 
 /* new users route */
