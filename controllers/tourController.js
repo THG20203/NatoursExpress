e481522ff62bd46189604bd818d58880c8f15df1
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-25 21:34:24 +0100

    sorting tourController

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
-const getAllTours = (req, res) => {
+exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
@@ -16,7 +16,7 @@ const getAllTours = (req, res) => {
   });
 };
 
-const getTour = (req, res) => {
+exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
   if (!tour) {
@@ -33,7 +33,7 @@ const getTour = (req, res) => {
   });
 };
 
-const createTour = (req, res) => {
+exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -52,7 +52,7 @@ const createTour = (req, res) => {
   );
 };
 
-const updateTour = (req, res) => {
+exports.updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -67,7 +67,7 @@ const updateTour = (req, res) => {
   });
 };
 
-const deleteTour = (req, res) => {
+exports.deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,13 +1,22 @@
 /* need to import the express module */
 const express = require('express');
+/* tourController now effectively an object */
+const tourController = require('./../controllers/tourController');
 
 /* calling it router rather than tourRouter because of convention */
 const router = express.Router();
 
 /* below only want route / cause we're in the router anyway */
-router.route('/').get(getAllTours).post(createTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 /* below only want id  cause we're in the router anyway */
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 /* export router - only one thing to export so module.exports */
 module.exports = router;
